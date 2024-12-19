29d2cc49284c706e5aa8d7ad1d904a560556d467
Nguyen van Phong <ngvanphong2012@gmail.com>
2024-12-19 18:31:01 +0700

    Add project files.

---
+++ KssRevitApi2/App.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KssRevitApi2
+{
+    public class App : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+           
+            return Result.Succeeded;
+        }
+    }
+}
