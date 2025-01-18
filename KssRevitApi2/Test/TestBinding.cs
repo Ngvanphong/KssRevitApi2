4291982bebcb1b5d5823b538ae6cf51a10776b02
Nguyen van Phong <ngvanphong2012@gmail.com>
2025-01-18 11:10:50 +0700

    5252252

--- KssRevitApi2/Test/TestBinding.cs
+++ KssRevitApi2/Test/TestBinding.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System.Collections.Generic;
@@ -16,188 +17,144 @@ namespace KssRevitApi2.Test
             UIDocument uiDoc = commandData.Application.ActiveUIDocument;
             Document doc = uiDoc.Document;
 
-            #region comment
-            //IEnumerable<ElementId> ids = uiDoc.Selection.GetElementIds();
+            XYZ firstPoint = uiDoc.Selection.PickPoint("Pick start point");
+            XYZ endPoint = uiDoc.Selection.PickPoint("Pick end point");
+
+            XYZ pointOfDim = uiDoc.Selection.PickPoint("Choose a point to put dim");
 
 
-            //FamilyInstance column = null;
-            //try
-            //{
-            //    Reference pickRef = uiDoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element, new ColumnFilter(), "Pick a column");
-            //    column = doc.GetElement(pickRef) as FamilyInstance;
-            //}
-            //catch { }
+            double xMin = Math.Min(firstPoint.X, endPoint.X);
+            double yMin = Math.Min(firstPoint.Y, endPoint.Y);
+            double zMin = Math.Min(firstPoint.Z, endPoint.Z);
 
-            //BoundingBoxXYZ boundingBox = column.get_BoundingBox(null);
+            double xMax = Math.Max(firstPoint.X, endPoint.X);
+            double yMax = Math.Max(firstPoint.Y, endPoint.Y);
+            double zMax = Math.Max(firstPoint.Z, endPoint.Z);
 
-            //List<FamilyInstance> listColumn = new List<FamilyInstance>();
-            //var listRefSelected = uiDoc.Selection.PickObjects(ObjectType.Element, new ColumnFilter(), "Pick a columns");
-
-            //var selectdRef = uiDoc.Selection.PickElementsByRectangle(new ColumnFilter(), "Pick a column");
-
-            //var selecREf2 = uiDoc.Selection.PickBox()
+            double extend = 10 / 304.8;
+            XYZ min = new XYZ(xMin - extend, yMin - extend, zMin - extend);
+            XYZ max = new XYZ(xMax + extend, yMax + extend, zMax + extend);
+            Outline outline = new Outline(min, max);
 
 
-            //FilteredElementCollector collection = new FilteredElementCollector(doc, doc.ActiveView.Id);
-            //collection = collection.OfCategory(BuiltInCategory.OST_Walls).WhereElementIsNotElementType();
-            //IEnumerable<Wall> walls = collection.ToElements().Cast<Wall>();
-
-            //List<Wall> listWallLevel2 = walls.Where(item =>
-            //{
-            //    Parameter paramter = item.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE);
-            //    ElementId levelId = paramter.AsElementId();
-            //    Level level = doc.GetElement(levelId) as Level;
-            //    return level.Name == "L2";
-            //}).ToList();
-
-            //var level1 = new FilteredElementCollector(doc).OfClass(typeof(Level)).First(x => x.Name == "L1");
-            //ElementId idPara = new ElementId(BuiltInParameter.WALL_HEIGHT_TYPE);
-            //FilterRule filterRule = ParameterFilterRuleFactory.CreateEqualsRule(idPara, level1.Id);
-            //ElementParameterFilter elemenentParaFilter = new ElementParameterFilter(filterRule);
-            //var listWall = collection.WherePasses(elemenentParaFilter).ToElements();
+            BoundingBoxIntersectsFilter boundingFilter = new BoundingBoxIntersectsFilter(outline);
+            ElementCategoryFilter categoryFilter = new ElementCategoryFilter(BuiltInCategory.OST_Walls);
+            LogicalAndFilter andFitler = new LogicalAndFilter(new List<ElementFilter> { boundingFilter, categoryFilter });
+            IEnumerable<Wall> walls = new FilteredElementCollector(doc, doc.ActiveView.Id).WherePasses(andFitler)
+                .OfClass(typeof(Wall)).Cast<Wall>();
 
 
-            //// collect all type of wall
-            //IEnumerable<WallType> listWallType = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Walls)
-            //    .WhereElementIsElementType().Cast<WallType>();
+            ReferenceArray referenceArray = new ReferenceArray();
+            foreach(Wall wall in walls)
+            {
+                var innerFaces = HostObjectUtils.GetSideFaces(wall, ShellLayerType.Interior);
+                if(innerFaces.Any()) 
+                    foreach (var item in innerFaces)
+                    {
+                        referenceArray.Append(item);
+                    }
 
-            //Wall wallFirst = listWall.First() as Wall;
+                var exteriorFaces = HostObjectUtils.GetSideFaces(wall, ShellLayerType.Exterior);
+                if (exteriorFaces.Any())
+                    foreach (var item in exteriorFaces)
+                    {
+                        referenceArray.Append(item);
+                    }
 
-            //Parameter parameterLength = wallFirst.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH);
-            //double lengthInch = 2;// don vi inch
+            }
 
+            XYZ directionWall = null;
+            foreach(Wall wall in walls)
+            {
+                Location location = wall.Location;
+                if (location!=null  && location is LocationCurve locaitonCurve)
+                {
+                    if(locaitonCurve.Curve is Line lineItem)
+                    {
+                        directionWall = lineItem.Direction.Normalize();
+                        break;
+                    }
+                }
 
-            //FormatOptions formatOption= doc.GetUnits().GetFormatOptions(SpecTypeId.Length);
-            //double lengthDiplayUnit = UnitUtils.ConvertFromInternalUnits(lengthInch, formatOption.GetUnitTypeId());
+            }
 
+            XYZ direcitonDim = directionWall.CrossProduct(doc.ActiveView.ViewDirection).Normalize();
 
+            Line line = Line.CreateUnbound(pointOfDim, direcitonDim);
 
-            //BoundingBoxXYZ boundingBox = wallFirst.get_BoundingBox(doc.ActiveView);
-            //XYZ min = boundingBox.Min;
-            //XYZ max = boundingBox.Max;
-
-            //double xMin = Math.Min(min.X, max.X);
-            //double yMin = Math.Min(min.Y, max.Y);
-            //double zMin = Math.Min(min.Z, max.Z);
-            //double xMax = Math.Max(min.X, max.X);
-            //double yMax = Math.Max(min.Y, max.Y);
-            //double zMax = Math.Max(min.Z, max.Z);
-
-
-            //Outline outline = new Outline(new XYZ(xMin, yMin, zMin), new XYZ(xMax, yMax, zMax));
-
-            //BoundingBoxIntersectsFilter boundingIntersecFilter = new BoundingBoxIntersectsFilter(outline);
-            //ExclusionFilter exculusionFilter = new ExclusionFilter(new List<ElementId> { wallFirst.Id });
-
-            //ElementCategoryFilter beamFilter = new ElementCategoryFilter(BuiltInCategory.OST_StructuralFraming);
-            //ElementCategoryFilter colFilter = new ElementCategoryFilter(BuiltInCategory.OST_StructuralColumns);
-            //LogicalOrFilter logicalOrFilter = new LogicalOrFilter(new List<ElementFilter> { beamFilter, colFilter });
-
-
-            //LogicalAndFilter logicalAndFilter = new LogicalAndFilter(new List<ElementFilter> { boundingIntersecFilter, exculusionFilter,
-            //    logicalOrFilter });
-
-
-
-            //var listIntersect = new FilteredElementCollector(doc, doc.ActiveView.Id).WherePasses(logicalAndFilter)
-            //    .ToElements();
-
-
-            ////IEnumerable<Wall> walls = colleciton.OfClass(typeof(Wall)).Cast<Wall>();
-
-
-            //FamilyInstance column = doc.GetElement(uiDoc.Selection.GetElementIds().First()) as FamilyInstance;
-
-            //XYZ rightVector = doc.ActiveView.RightDirection;
-            //XYZ upVector = doc.ActiveView.UpDirection;
-            //XYZ viewVector = doc.ActiveView.ViewDirection;
-
-            //XYZ vectorMove = new XYZ(-1, 0, 0) * 500 / 304.8;
-            //using (Transaction t = new Transaction(doc, "MoveElement"))
-            //{
-            //    t.Start();
-            //    ElementTransformUtils.MoveElement(doc, column.Id, vectorMove);
-            //    t.Commit();
-            //}
-
-            //Location locationCol = column.Location;
-            //XYZ locationPoint = (locationCol as LocationPoint).Point;
-            //Line lineAxis = Line.CreateUnbound(locationPoint, viewVector);
-            //using(Transaction t2= new Transaction(doc, "Rotate"))
-            //{
-            //    t2.Start();
-            //    ElementTransformUtils.RotateElement(doc, column.Id, lineAxis, Math.PI / 6);
-            //    t2.Commit();
-            //}
-            #endregion
-
-            Wall wall = doc.GetElement(uiDoc.Selection.GetElementIds().First()) as Wall;
-            LocationCurve locationCurve = wall.Location as LocationCurve;
-            Curve curve = locationCurve.Curve;
-
-            //Arc arc = curve as Arc;
-            //XYZ pointCenter = arc.Center;
-
-            //XYZ viewVector = doc.ActiveView.ViewDirection.Normalize();
-            //Curve curveOfffset=  curve.CreateOffset(500 / 304.8, viewVector);
-
-            //Curve curveReverse = curve.CreateReversed();
-
-            //Transform transformRight1000 = Transform.CreateTranslation(doc.ActiveView.RightDirection.Normalize() * 1000 / 304.8);
-            //Curve cureMoveRight1000= curve.CreateTransformed(transformRight1000);
-
-
-            //Transform transformRotate45 = Transform.CreateRotationAtPoint(XYZ.BasisZ, Math.PI/4, pointCenter);
-
-            // Curve curveRotate = curve.CreateTransformed(transformRotate45);
-
-            //Transform tasnformMove = Transform.CreateTranslation(pointCenter-)
-
-
-            double startPara = curve.GetEndParameter(0);
-            XYZ startPoint = curve.GetEndPoint(0);
-            double endPara = curve.GetEndParameter(1);
-            XYZ endPoint = curve.GetEndPoint(1);
-
-            double rate = 1000 / 304.8 / curve.Length;
-            double startParaAfter = startPara - rate * (endPara - startPara);
-            double endParaAfter = endPara + rate * (endPara - startPara);
-
-
-            curve.MakeBound(startParaAfter, endParaAfter);
-
-            using (Transaction t = new Transaction(doc, "RotateWall"))
+            Dimension dimension = null;
+            using(Transaction t= new Transaction(doc, "CreateDim"))
             {
                 t.Start();
-                (wall.Location as LocationCurve).Curve = curve;
+                dimension= doc.Create.NewDimension(doc.ActiveView, line, referenceArray);
                 t.Commit();
             }
-            //ElementTransformUtils.RotateElement(doc, wall.Id, )
-            Curve curve2 = null;
-            SetComparisonResult compareResult = curve.Intersect(curve2, out IntersectionResultArray resultArray);
-            if (compareResult == SetComparisonResult.Overlap)
+
+            DimensionSegmentArray segementArray = dimension.Segments;
+            if (segementArray == null)
             {
-                for (int i = 0; i < resultArray.Size; i++)
-                {
-                    XYZ pontIntersect = resultArray.get_Item(i).XYZPoint;
-                }
+                ReferenceArray refereenceArray1 = dimension.References;
+                double value = dimension.Value.Value;
+                string valueString = dimension.ValueString;
             }
-            XYZ pointAtCenter= curve.Evaluate((startPara+ endPara)/2,false);
-            Transform derivative = curve.ComputeDerivatives((startPara + endPara) / 2, false);
-            XYZ vectorM=  derivative.BasisX.Normalize() * 1000 / 304.8;
-            Transform transformM = Transform.CreateTranslation(vectorM);
-            var listCopy= ElementTransformUtils.CopyElement(doc, wall.Id, new XYZ(1000 / 304, 0, 0));
-            Curve curve1 = null;
-            double offset = 1000 / 304.8;
-            double paraStart = curve1.GetEndParameter(0);
-            double paraEnd = curve1.GetEndParameter(1);
-            double rateLeft = (curve1.Length / 2 - offset) / curve1.Length;
-            double paraLeft = paraStart + rateLeft * (paraEnd - paraStart);
+            else
+            {
+                ReferenceArray referenceSegement = dimension.References;
+                var iteratorReference = referenceSegement.GetEnumerator();
+                iteratorReference.Reset();
 
-            XYZ pointLEft = curve1.Evaluate(paraLeft, false);
+                var iterator = segementArray.GetEnumerator();
 
+                iteratorReference.MoveNext();
+
+                iterator.Reset();
+                while (iterator.MoveNext())
+                {
+                    DimensionSegment segment = iterator.Current as DimensionSegment;
+                    double value = segment.Value.Value;
+                    string valueString = segment.ValueString;
+
+                    Reference startRef = iteratorReference.Current as Reference;
+                    iteratorReference.MoveNext();
+                    Reference endRef = iteratorReference.Current as Reference;
+
+                    ReferenceArray refArrayItem = new ReferenceArray();
+                    refArrayItem.Append(startRef); refArrayItem.Append(endRef);
+                   
+                    using(Transaction t= new Transaction(doc, "DivideDim"))
+                    {
+                        t.Start();
+                        doc.Create.NewDimension(doc.ActiveView, line, refArrayItem);
+                        t.Commit();
+                    }
+
+
+                }
+
+            }
+
+            
             return Result.Succeeded;
         }
+
+
+        public static  void  GetGeomtryInstance(GeometryInstance geoInst,List<Solid> listSolidResut)
+        {
+            GeometryElement geoElement = geoInst.GetInstanceGeometry();
+            foreach(GeometryObject geoObj  in geoElement)
+            {
+                if(geoObj is Solid solid && solid.Volume > 0.0001)
+                {
+                    listSolidResut.Add(solid);
+                }
+                else
+                {
+                    GeometryInstance geoItemInst = geoObj as GeometryInstance;
+                    if (geoItemInst != null) GetGeomtryInstance(geoItemInst, listSolidResut);
+                }
+            }
+        }
+        
     }
 
     public class ColumnFilter : ISelectionFilter
