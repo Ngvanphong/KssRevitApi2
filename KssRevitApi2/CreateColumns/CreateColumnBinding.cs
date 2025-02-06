d8bbc5a83eb9e6249235c535c6a498afc2084054
Nguyen van Phong <ngvanphong2012@gmail.com>
2025-02-06 19:13:47 +0700

    5252525

--- KssRevitApi2/CreateColumns/CreateColumnBinding.cs
+++ KssRevitApi2/CreateColumns/CreateColumnBinding.cs
@@ -14,15 +14,73 @@ namespace KssRevitApi2.CreateColumns
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
             Document doc = commandData.Application.ActiveUIDocument.Document;
-            CreateColumnAppShow.ShowForm();
 
-            List<FamilySymbol> listTypeCol = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_StructuralColumns)
-                .WhereElementIsElementType().OfClass(typeof(FamilySymbol)).Cast<FamilySymbol>().ToList();
+            List<Curve> listCurveEdge = new List<Curve>();
+            while (true)
+            {
+                try
+                {
+                    Reference pickRef = uiDoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Edge, "Pick edge of host");
+                    Edge edge = doc.GetElement(pickRef).GetGeometryObjectFromReference(pickRef) as Edge;
+                    if (edge != null && edge.AsCurve() != null) listCurveEdge.Add(edge.AsCurve());
+                }
+                catch { break; }
+            }
+            List<Curve> listCurveOrder = new List<Curve>();
 
-            CreateColumnAppShow.FormCreateColumn.DataContext = new CreateColumnData();
-            CreateColumnAppShow.FormCreateColumn.comboboxTypeColumn.ItemsSource= listTypeCol;
-               
+            XYZ startPointCurvelop = null;
+            foreach (Curve curve in listCurveEdge)
+            {
+                List<XYZ> listPointCurve = new List<XYZ> { curve.GetEndPoint(0), curve.GetEndPoint(1) };
+                foreach (XYZ point in listPointCurve)
+                {
+                    bool hasStart = true;
+                    foreach (Curve curveCheck in listCurveEdge)
+                    {
+                        if (curveCheck == curve) continue;
+                        List<XYZ> listPointCheck = new List<XYZ> { curveCheck.GetEndPoint(0), curveCheck.GetEndPoint(1) };
+                        foreach (XYZ pointCheck in listPointCheck)
+                        {
+                            if (point.IsAlmostEqualTo(pointCheck,0.0001))
+                            {
+                                hasStart = false;
+                                break;
+                            }
+                        }
+                        if (!hasStart) break;
+                    }
+                    if (hasStart)
+                    {
+                        startPointCurvelop = point;
+                        break;
+                    }
+
+                }
+                if (startPointCurvelop != null) break;
+
+            }
+
+            CurveLoop curveloop = new CurveLoop();
+            XYZ pointNext = startPointCurvelop;
+            foreach(Curve  curve in listCurveEdge)
+            {
+                XYZ sp = curve.GetEndPoint(0);
+                XYZ ep= curve.GetEndPoint(1);
+                if(sp.IsAlmostEqualTo(pointNext, 0.0001))
+                {
+                    curveloop.Append(curve);
+                }
+                if(ep.IsAlmostEqualTo(pointNext, 0.0001))
+                {
+                    Curve curveRevert = curve.CreateReversed();
+                }
+            }
+
+
+
+
             return Result.Succeeded;
         }
     }
--- KssRevitApi2/CreateColumns/PickPointHandler.cs
+++ KssRevitApi2/CreateColumns/PickPointHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
@@ -14,8 +15,42 @@ namespace KssRevitApi2.CreateColumns
         {
             UIDocument uiDoc = app.ActiveUIDocument;
             Document doc = uiDoc.Document;
-            XYZ point = null;
-           
+
+            Floor floor = null;
+            Reference refTopFace = HostObjectUtils.GetTopFaces(floor).First();
+            Face topFace = doc.GetElement(refTopFace).GetGeometryObjectFromReference(refTopFace) as Face;
+            Line line = null;
+            AreaReinforcementType.CreateDefaultAreaReinforcementType(doc);
+
+
+            CurveLoop curveloop = null;
+            Curve curve1 = null;
+            Curve curve2 = null;
+            int index = 0;
+            foreach(Curve curveIem in curveloop)
+            {
+                if (index == 0) curve1 = curveIem;
+                else if (index == 1) curve2 = curveIem;
+            }
+
+            CurveLoop newCurveLoop1 = new CurveLoop();
+            newCurveLoop1.Append(curve1); newCurveLoop1.Append(curve2);
+
+            CurveLoop newCurveloop2 = CurveLoop.CreateViaOffset(newCurveLoop1, 1000 / 304.8, XYZ.BasisZ);
+
+            Curve curve11 = null;
+            Curve curve22 = null;
+
+            Curve curve4 = curve22.CreateReversed();
+
+            Curve curve5 = curve11.CreateReversed();
+
+            Curve curve3 = Line.CreateBound(curve2.GetEndPoint(1), curve4.GetEndPoint(0));
+
+       
+
+
+
 
         }
 
