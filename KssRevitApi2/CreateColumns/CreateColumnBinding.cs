35676cf90af22b4ecbbf9faa8a8e1c7af9a5e098
Nguyen van Phong <ngvanphong2012@gmail.com>
2025-03-25 19:52:00 +0700

    5252525

--- KssRevitApi2/CreateColumns/CreateColumnBinding.cs
+++ KssRevitApi2/CreateColumns/CreateColumnBinding.cs
@@ -13,112 +13,80 @@ namespace KssRevitApi2.CreateColumns
     [Transaction(TransactionMode.Manual)]
     public class CreateColumnBinding : IExternalCommand
     {
+        public static CreateDimensionListening _updater = null;
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             UIDocument uiDoc = commandData.Application.ActiveUIDocument;
             Document doc = commandData.Application.ActiveUIDocument.Document;
 
-            IEnumerable<ElementId> idsSelected = uiDoc.Selection.GetElementIds();
-            List<FamilyInstance> listBeam= new List<FamilyInstance>();
-            foreach(ElementId id in idsSelected)
+            //using(Transaction t= new Transaction(doc, "Create"))
+            //{
+            //    t.Start();
+
+            //    FailureHandlingOptions familyHandlerOption = t.GetFailureHandlingOptions();
+            //    familyHandlerOption.SetFailuresPreprocessor(new WarningHandle());
+            //    t.SetFailureHandlingOptions(familyHandlerOption);
+
+
+            //    Wall.Create(doc, new List<Curve>(), true);
+
+
+            //    t.Commit(familyHandlerOption);
+            //}
+
+            // listening
+
+            UIApplication uiapp = commandData.Application;
+            var app = uiapp.Application;
+            if (_updater == null)
             {
-                FamilyInstance item = doc.GetElement(id) as FamilyInstance;
-                if(item != null && item.Category != null && item.Category.Id.Value == (long)BuiltInCategory.OST_StructuralFraming)
-                {
-                    if(item.Location != null && item.Location is LocationCurve) listBeam.Add(item);
-                }
+                _updater = new CreateDimensionListening(app.ActiveAddInId);
+                UpdaterRegistry.RegisterUpdater(_updater);
+
+                ElementCategoryFilter f = new ElementCategoryFilter(BuiltInCategory.OST_Dimensions);
+                UpdaterRegistry.AddTrigger(_updater.GetUpdaterId(), f, Element.GetChangeTypeElementAddition());
             }
-
-            List<XYZ> listPoint = new List<XYZ>();
-            foreach(FamilyInstance beam in listBeam)
+            else
             {
-                Curve curveBeam = (beam.Location as LocationCurve).Curve;
-                List<XYZ> listPointBeam = new List<XYZ>
+                try
                 {
-                    curveBeam.GetEndPoint(0), curveBeam.GetEndPoint(1)
-                };
-                
-                foreach(XYZ point in listPointBeam)
-                {
-                    if(!listPoint.Exists(x=>x.IsAlmostEqualTo(point,0.001))) listPoint.Add(point);
+                    UpdaterRegistry.UnregisterUpdater(_updater.GetUpdaterId());
+                    _updater = null;
                 }
-            }
-
-            // arrange points
-            List<List<XYZ>> listPointSort= new List<List<XYZ>>();
-            foreach(XYZ point in listPoint)
-            {
-                bool isExist = false;
-                foreach(List<XYZ> listCols in listPointSort)
-                {
-                    if (Math.Abs(listCols[0].X - point.X)<0.001)
-                    {
-                        listCols.Add(point);
-                        isExist = true;
-                        break;
-                    }
-                }
-                if(!isExist)
-                {
-                    listPointSort.Add(new List<XYZ> { point });
-                }
-            }
-
-            listPointSort = listPointSort.OrderBy(listCol => listCol[0].X).ToList();
-
-            List<List<XYZ>> listPointSortFinish = new List<List<XYZ>>();
-            foreach (List<XYZ> listCol in listPointSort)
-            {
-                var newlistCol = listCol.OrderBy(item => item.Y).ToList();
-                listPointSortFinish.Add(newlistCol);
-            }
-
-            List<Line> listLine = new List<Line>();
-            for(int col= 0; col< listPointSortFinish.Count; col += 2)
-            {
-                for(int row= 0; row< listPointSortFinish[col].Count; row += 2)
-                {
-                    XYZ pointOrigin = listPointSortFinish[col][row];
-                    XYZ left = null;
-                    if(col-1>=0 && row+1< listPointSortFinish[col].Count)
-                    {
-                        left= listPointSortFinish[col - 1][row + 1];
-                    }
-
-                }
+                catch { }
 
             }
 
-
-            Floor floor = null;
-            //floor.get_Parameter(BuiltInParameter.);
-            floor.LookupParameter("Para1");
-            //TagMode.
-            ///.Revit.DB.IndependentTag.Create()
-
-            //UnitUtils.Convert()
-            //BoundingBoxIntersectsFilter boxInter= new BoundingBoxIntersectsFilter()
-            foreach(Parameter para in floor.Parameters)
-            {
-                Definition definition = para.Definition;
-                if (definition != null)
-                {
-                    if(definition.GetDataType().TypeId == SpecTypeId.Volume.TypeId)
-                    {
-
-                    }
-                }
-            }
 
 
             return Result.Succeeded;
         }
 
-        public static void ExtendLine(ref Line curve, double extend)
+       
+    }
+    public class WarningHandle : IFailuresPreprocessor
+    {
+        public FailureProcessingResult PreprocessFailures(FailuresAccessor failuresAccessor)
         {
-            double sp = curve.GetEndParameter(0); double ep = curve.GetEndParameter(1);
-            double rate = extend / curve.Length;
-            curve.MakeBound(sp - rate * (ep - sp), ep + rate * (ep - sp));
+            foreach(FailureMessageAccessor warningErr in failuresAccessor.GetFailureMessages())
+            {
+                if(warningErr.GetSeverity() == FailureSeverity.Warning)
+                {
+                    failuresAccessor.DeleteWarning(warningErr);
+                    FailureDefinitionId idOfWaring= warningErr.GetFailureDefinitionId();
+                    if(idOfWaring == BuiltInFailures.WallFailures.WallNegativeHeight)
+                    {
+
+                    }
+                    return FailureProcessingResult.Continue;
+                }
+                else 
+                {
+                    failuresAccessor.DeleteWarning(warningErr);
+                    return FailureProcessingResult.ProceedWithRollBack;
+                }
+            }
+            return FailureProcessingResult.Continue;
         }
     }
 }
---
+++ KssRevitApi2/CreateColumns/CreateDimensionListening.cs
@@ -0,0 +1,64 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KssRevitApi2.CreateColumns
+{
+    public class CreateDimensionListening : IUpdater
+    {
+        private AddInId _appId;
+        private UpdaterId _updaterId;
+        public CreateDimensionListening(AddInId appId)
+        {
+            _appId = appId;
+            _updaterId = new UpdaterId(appId, new Guid("f781a8da-bb26-48f2-8219-c086acd8b7cf"));
+        }
+        public void Execute(UpdaterData data)
+        {
+            Document doc = data.GetDocument();
+            IEnumerable<ElementId> idsAdd= data.GetAddedElementIds();
+            //IEnumerable<ElementId> idsModify= data.GetModifiedElementIds();
+            if(idsAdd.Any())
+            {
+                foreach(ElementId id in idsAdd)
+                {
+                    Dimension dimension= doc.GetElement(id) as Dimension;
+                    {
+                        if(dimension != null)
+                        {
+
+                            /// todo 
+                            /// 
+
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+        public string GetAdditionalInformation()
+        {
+            return "NguyenVanPhong";
+        }
+
+        public ChangePriority GetChangePriority()
+        {
+            return ChangePriority.Annotations;
+        }
+
+        public UpdaterId GetUpdaterId()
+        {
+            return _updaterId;
+        }
+
+        public string GetUpdaterName()
+        {
+            return "ListenCreateDimension";
+        }
+    }
+}
